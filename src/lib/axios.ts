c6a0f0c100b8acda5bfdbce63be8b116da83b321
André Chamis <andrechamis21@gmail.com>
2022-09-09 15:20:03 -0300

    Fixed github key issue

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Authorization: process.env.REACT_APP_GITHUB_KEY!,
+    Authorization: import.meta.env.REACT_APP_GITHUB_KEY,
   },
 })
